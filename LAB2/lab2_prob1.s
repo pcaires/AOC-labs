89513535a62184945d6675f973f51725cf8f196f
BrunoMid <91807241+BrunoMid@users.noreply.github.com>
2022-01-25 15:58:17 +0000

    Update lab2_prob1.s

--- LAB2/lab2_prob1.s
+++ LAB2/lab2_prob1.s
@@ -9,60 +9,60 @@ n:		.word  0
 # Program section
 .text
 		                        # NOTE: Upon start, the Global-Pointer (gp=x3) points to the beginning of .data section
-	addi    x11, x3, 0		# x11 - a's left index
-	nop
-	nop
-	addi    x13, x11, 48		# x13 - b's left index
- 	nop
-	nop
-	addi	x12, x13, -4		# x12 - a's right index
+        addi    x11, x3, 0		# x11 - a's left index
+        nop
+        nop
+        addi    x13, x11, 48		# x13 - b's left index
+        nop
+        nop
+        addi	x12, x13, -4		# x12 - a's right index
 		
-	lw 	x14, 100(x3)		# x14 - n - index distance accumulator
-	lw	x15, 96(x3)		# x15 - x
-	li 	x16, 0			# x16 - i
+        lw 	x14, 100(x3)		# x14 - n - index distance accumulator
+        lw	x15, 96(x3)		# x15 - x
+        li 	x16, 0			# x16 - i
 
-	nop 
-	nop 
+        nop 
+        nop 
 	
 while:  add	x20, x13, x16		# x20 = &b[i]
-	nop
-        nop
-	lw	x21, 0(x20)		# x21 = b[i]
         nop
         nop
-	blez	x21, end		# if b[i] <= 0 end the loop
+        lw	x21, 0(x20)		# x21 = b[i]
+        nop
+        nop
+        blez	x21, end		# if b[i] <= 0 end the loop
 
-	lw 	x22, 0(x11)		# x22 = a[i]
-	lw 	x23, 0(x12)		# x23 = a[N-1-i]
-	nop
+        lw 	x22, 0(x11)		# x22 = a[i]
+        lw 	x23, 0(x12)		# x23 = a[N-1-i]
+        nop
         nop
         add	x22, x22, x23		# x22 = a[i] + a[N-1-i]
         nop
         nop
-	mul 	x15, x15, x22		# x15 = x15*x22 (x *= a[i] + a[N-1-i])
+        mul 	x15, x15, x22		# x15 = x15*x22 (x *= a[i] + a[N-1-i])
 
-	sub	x22, x12, x11		# x22 = 4*((N-1-i)-i)
-	nop
+        sub	x22, x12, x11		# x22 = 4*((N-1-i)-i)
+        nop
         nop
         srai	x22, x22, 2		# x22 = x22/4
         nop
         nop
-	add	x14, x14, x22		# n += x22
+        add	x14, x14, x22		# n += x22
 
-	addi 	x16, x16, 4		# i++
+        addi 	x16, x16, 4		# i++
 
-	addi	x11, x11, 4
-	addi	x12, x12, -4
-	jal 	x0, while
+        addi	x11, x11, 4
+        addi	x12, x12, -4
+        jal 	x0, while
 
 end:	sw 	x14, 100(x3)		# store n's final value
-	sw 	x15, 96(x3)		# store x's final value	
+        sw 	x15, 96(x3)		# store x's final value	
 
-	addi	a7, x0, 10
+        addi	a7, x0, 10
         nop
         nop
         nop
-	ecall
+        ecall
 
 
 # Expected result: M[x] = 1270080 = 136140h
